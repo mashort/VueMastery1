88a17523f66289aa3d608081776d4d25c129212b
martins <martin.short@qnrl.com>
2019-01-22 16:53:09 +0000

    Add lesson 3 and 4 code

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg'
+        image: './assets/vmSocks-green-onWhite.jpg',
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ]
     }
 })
